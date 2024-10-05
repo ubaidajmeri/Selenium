ee200e33f7da8012ed6518df42a8f8f05be82eb6
Ubaid Ajmeri <ubedajmeri@gmail.com>
2024-10-05 12:58:48 +0530

    Initial Commit

---
+++ src/main/java/as1/app.java
@@ -0,0 +1,15 @@
+package as1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		
+		driver.navigate().to("https://github.com/ubedahmed");
+	}
+
+}
